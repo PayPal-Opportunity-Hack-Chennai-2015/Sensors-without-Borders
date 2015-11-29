5e2da1ced44b3574f6a1a1ec91e844ea5ed32d3d
siddick <siddick@gmail.com>
2015-11-29 10:14:04 +0530

    Added dataTable

--- cats/public/js/app.js
+++ cats/public/js/app.js
@@ -4,19 +4,25 @@
 requirejs.config({
     paths: {
         jquery: '../components/jquery/dist/jquery',
-        bootstrap: '../components/bootstrap/dist/js/bootstrap'
+        bootstrap: '../components/bootstrap/dist/js/bootstrap',
+        datatables: "../components/datatables/media/js/jquery.dataTables.min",
+        tableTools: "../components/datatables-tabletools/js/dataTables.tableTools",
+        dataTablesBootstrap: "../components/Plugins/integration/bootstrap/3/dataTables.bootstrap.min",
     },
     shim: {
-        bootstrap: ['jquery']
+        bootstrap: ['jquery'],
+        datatables: ["jquery"],
+        dataTablesBootstrap: ["datatables", "bootstrap"],
+        tableTools: ["dataTablesBootstrap"]
     }
 });
 
 
-require(['bootstrap'], function () {
+require(['bootstrap', 'tableTools'], function () {
 
     var app = {
         initialize: function () {
-            // Your code here
+			$('table.table').DataTable();
         }
     };
 
